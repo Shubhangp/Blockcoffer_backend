1ed332ec416473f82f76ff355a8f26c864d78925
Shubhang Pandey <86318886+Shubhangp@users.noreply.github.com>
2025-02-01 11:51:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(cors({
     origin: '*'
-}));
+})); 
 app.use((req, res, next) => {
     console.log('Hello from the middleware 👋');
     next();
